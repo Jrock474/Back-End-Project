d55a6d05be4498bddaa29de322edd6d98841c868
Santiago Felizzola <santi@LAPTOP-DQG00BQ5.localdomain>
2023-09-07 15:17:25 -0400

    added get routes for user expenses & income

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@ const sqlize = require('sequelize');
 const app = express();
 const pg = require('pg');
 const winston = require('winston');
-const {Users, Expense_Transaction, Income_Transaction } = require('./models')
+const { Users, Expense_Transaction, Income_Transaction } = require('./models')
 const port = 3000
 const bodyParser = require('body-parser')
 const bcrypt = require('bcrypt')
@@ -21,29 +21,29 @@ const logger = winston.createLogger({
     new winston.transports.File({ filename: 'combined.log' }),
   ],
 });
-app.all('*',(req,res, next)=>{
+app.all('*', (req, res, next) => {
   logger.info({
-      level: 'info',
-      method:req.method,
-      body:req.body,
-      url:req.url,
-      parameters:req.params,
-      timestamp:new Date().toLocaleString()
+    level: 'info',
+    method: req.method,
+    body: req.body,
+    url: req.url,
+    parameters: req.params,
+    timestamp: new Date().toLocaleString()
   })
   next()
 })
 
-app.get('/users-all',async(req,res)=>{
+app.get('/users-all', async (req, res) => {
   const allUsers = await Users.findAll()
   res.send(allUsers)
 })
 
-app.get('/',(req,res)=>{
-    res.render('sign-up')
+app.get('/', (req, res) => {
+  res.render('sign-up')
 })
 
 
-app.get('/users', async(req,res) => {
+app.get('/users', async (req, res) => {
   const allUsers = await Users.findAll();
   res.send(allUsers)
   console.log(`That's all folks!`)
@@ -52,70 +52,71 @@ app.get('/users', async(req,res) => {
 
 app.post('/sign-up', async (req, res) => {
   const { Name, Email, Password, ReEnterPassword } = req.body;
-  
-  if (Password !== ReEnterPassword){
+
+  if (Password !== ReEnterPassword) {
     return res.render('sign-up', { error: 'Passwords must match' });
   }
 
- //Encrypts Password
-const saltRounds = 10;
-bcrypt.hash(Password, saltRounds, async(err, hash) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  console.log('Hashed password:', hash);
+  //Encrypts Password
+  const saltRounds = 10;
+  bcrypt.hash(Password, saltRounds, async (err, hash) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log('Hashed password:', hash);
 
-  // If successful, inserts Data into Database as a new User
-  try {
-    const newUser = await Users.create({
-       Name: Name,
-       Email: Email,
-       Password: hash,
-       ReEnterPassword: hash
-     });
-     res.send(newUser)
-   } catch (error) {
-       console.error(error);
-       return res.render('sign-up', { error: 'An error occurred during registration' });
-   }
-    
-  
-});
-// Logging and rendering 'register' view after successful registration or error handling
+    // If successful, inserts Data into Database as a new User
+    try {
+      const newUser = await Users.create({
+        Name: Name,
+        Email: Email,
+        Password: hash,
+        ReEnterPassword: hash
+      });
+      res.send(newUser)
+    } catch (error) {
+      console.error(error);
+      return res.render('sign-up', { error: 'An error occurred during registration' });
+    }
+
+
+  });
+  // Logging and rendering 'register' view after successful registration or error handling
   console.log({
     Name: Name,
     Email: Email,
     Password: Password,
     ReEnterPassword: ReEnterPassword,
   });
-  
+
 })
 
 // Sign in for Returning Users
 app.post('/sign-in', (req, res) => {
-    const Email = req.body.Email;
-    // const password = req.body.Password;
-    const returningUser = Users.findOne({
-        where:{
-            Email:Email, 
-    }})
-    if(!returningUser){
-        return res.status(400).send('invalid login');
+  const Email = req.body.Email;
+  // const password = req.body.Password;
+  const returningUser = Users.findOne({
+    where: {
+      Email: Email,
     }
-    res.render('dashbord')
+  })
+  if (!returningUser) {
+    return res.status(400).send('invalid login');
+  }
+  res.render('dashbord')
 
-    bcrypt.compare(userEnteredPassword, storedHashedPassword, (err, result) => {
-      if (err) {
-        console.error(err);
-        return;
-      }
-      if (result) {
-        console.log('Passwords match!');
-      } else {
-        console.log('Passwords do not match.');
-      }
-    });
+  bcrypt.compare(userEnteredPassword, storedHashedPassword, (err, result) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    if (result) {
+      console.log('Passwords match!');
+    } else {
+      console.log('Passwords do not match.');
+    }
+  });
 
 })
 
@@ -130,14 +131,14 @@ app.post('/sign-in', (req, res) => {
 //   res.send(updatedFinance)
 // })
 
-app.delete('/user/email',async(req,res)=>{
+app.delete('/user/email', async (req, res) => {
   await Users.destroy({
-      where: {
-          id: req.params.id
-      }
-    });
-    res.send('User has been deleted')
-    console.log(Users)
+    where: {
+      id: req.params.id
+    }
+  });
+  res.send('User has been deleted')
+  console.log(Users)
 
 })
 
@@ -199,6 +200,20 @@ app.post('/addIncome/:UserID', async (req, res) => {
   }
 });
 
+
+app.get('/userIncome/:UserID', async(req, res) => {
+  
+  const userIncome = await Income_Transaction.findAll({where: {UserID: req.params.UserID }});
+    res.send(userIncome)
+
+})
+
+app.get('/userExpense/:UserID', async(req, res) => {
+  
+  const userExpense = await Expense_Transaction.findAll({where: {UserID: req.params.UserID }});
+    res.send(userExpense)
+
+})
 
 
 app.listen(port, () => {
