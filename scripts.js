2c566afb6d191de9a9acc78588fb042ced6db6e6
Azure_Moon <jordanw0412@gmail.com>
2023-09-07 10:23:10 -0400

    added ejs changes and linked to DB

--- scripts.js
+++ scripts.js
@@ -5,9 +5,14 @@ const pg = require('pg');
 const winston = require('winston');
 const {Users} = require('./models')
 const port = 3000
+const bodyParser = require('body-parser')
 
+app.set('view engine', 'ejs');
 app.use(express.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 
+// parse application/json
+app.use(bodyParser.json())
 const logger = winston.createLogger({
   level: 'info',
   format: winston.format.json(),
@@ -30,8 +35,8 @@ app.all('*',(req,res, next)=>{
 })
 
 
-app.get('/Finances',(req,res)=>{
-  res.send('index')
+app.get('/',(req,res)=>{
+    res.render('login')
 })
 // app.get('/user/:id',async(req,res)=>{
 //   const budgetMoney = await user.findAll(
@@ -49,33 +54,42 @@ app.get('/Finances',(req,res)=>{
 // })
 
 app.post('/sign-up', async (req, res) => {
-const newUser = await Users.create({
-      
-          Name: req.body.Name,
-          Email: req.body.Email,
-          Password: req.body.Password,
-          ReEnterpassword: req.body.ReEnterpassword,
-      
-  })
-  res.send(newUser)
-  console.log(newUser)
+  const { Name, Email, Password, ReEnterPassword } = req.body;
+  try {
+   const newUser = await Users.create({
+      Name: Name,
+      Email: Email,
+      Password: Password,
+      ReEnterPassword: ReEnterPassword
+    });
+    res.send(newUser)
+  } catch (error) {
+    console.error(error);
+    return res.render('sign-up', { error: 'An error occurred during registration' });
+  }
+
+  // Logging and rendering 'register' view after successful registration or error handling
+  console.log({
+    Name: Name,
+    Email: Email,
+    Password: Password,
+    ReEnterPassword: ReEnterPassword,
+  });
   
 })
 
 app.post('/sign-in', (req, res) => {
-    const email = req.body.Email;
-    const password = req.body.Password;
+    const Email = req.body.Email;
+    // const password = req.body.Password;
     const returningUser = Users.findOne({
         where:{
-            email:email, 
+            Email:Email, 
     }})
     if(!returningUser){
         return res.status(400).send('invalid login');
     }
-
-
-
-} )
+    res.render('dashbord')
+})
 
 // app.put('/finances/:id',async(req,res)=>{
 //   const updateFinance = await finance.update({ income: req.body.income, expenses: req.body.expenses, savings: req.body.savings, surplus_deficit: req.body.surplus_deficit  },
